0bc5e22e71aabf052b249df6ff25cc94609c1073
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-04-01 19:31:32 +0200

    event abstract

--- Events/Event.h
+++ Events/Event.h
@@ -1,3 +1,16 @@
 #pragma once
+#include "../DataStructures/Date.h"
 
-class Event {};
+class Event {
+protected:
+	Date event_time;
+	int ID;
+
+public:
+	virtual void Excute() = 0;
+	virtual Date event_time() =0;
+
+
+
+
+};
