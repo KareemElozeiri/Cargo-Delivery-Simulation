75da8195a155c93b2e8e95d1c26c480f7e5ef7fe
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-04-01 15:54:46 +0200

    date all done

--- DataStructures/Date.cpp
+++ DataStructures/Date.cpp
@@ -1,1 +1,47 @@
 #include "Date.h"
+
+Date::Date()
+{
+	day = 0;
+	hour = 0;
+}
+
+Date::Date(int p_day, int p_hour)
+{
+	day = p_day;
+	hour = p_hour;
+}
+
+Date::Date(const Date& member)
+{
+	day = member.day;
+	hour = member.hour;
+}
+
+Date Date::operator+(Date secondDate)
+{
+	Date result;
+	result.day = day + secondDate.day;
+	result.hour = hour + secondDate.hour;
+	return result;
+}
+
+int Date::getDay()
+{
+	return day;
+}
+
+int Date::getHour()
+{
+	return hour;
+}
+
+void Date::setDay(int p_day)
+{
+	day = p_day;
+}
+
+void Date::setHour(int p_hour)
+{
+	hour = p_hour;
+}
--- DataStructures/Date.h
+++ DataStructures/Date.h
@@ -4,9 +4,17 @@ class Date
 	int day;
 	int hour;
 
+public:
 	Date();
 	Date(int p_day, int p_hour);
+	Date(const Date &member);
+	Date operator + (Date secondDate);
 
+	int getDay();
+	int getHour();
+	void setDay(int d);
+	void setHour(int h);
+	
 
 
 };
