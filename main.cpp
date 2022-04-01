ec11eefe0364e650bdfb4b4f7facdef09a2994af
KareemElozeiri <K.elozeiri@gmail.com>
2022-04-01 16:31:09 +0200

    Added Defs.h file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-
+#include "Defs.h"
 
 int main()
 {
-	
-	std::cout << "Reda el bahrawyyyyyyyy" << std::endl;
-	return 0;
+	Time t0(7, 6);
+	Time t1(6, 6);
+	Time t3 = t0 - t1;
+	std::cout << t3.getDay() << " " << t3.getHour();
 }
 
