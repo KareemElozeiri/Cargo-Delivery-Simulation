5d54e0690620e23824bcfd1770dd07eee177f2e5
Moustafa Ashamwy <mouashmawy@gmail.com>
2022-04-20 12:37:00 +0200

    promotionEvent: added constructor

--- Events/PromotionEvent.h
+++ Events/PromotionEvent.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "Event.h"
+
 class PromotionEvent: public Event
 {
 private:
 	double ExtraMoney; // Extra money for promotion
+	Time ED;
+	int ID_normal_cargo;
+
 
 public:
+
+	PromotionEvent(Time ED, int ID_normal_cargo, float ExtraMoney=0);
 	virtual void Execute();
 };
 
